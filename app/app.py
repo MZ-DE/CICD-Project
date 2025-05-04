79bbb4ab13ed0da1ad340112d0efb360712f6074
MZ-DE <zishan.mohammad@protonmail.com>
2025-05-04 14:36:30 +0200

    fix

--- app/app.py
+++ app/app.py
@@ -11,6 +11,10 @@ def index():
 def wise_quotes():
     return render_template("gen.html")
 
-@app.route("/female wisdom")
+@app.route("/female-wisdom")
 def fe_wise_quotes():
     return render_template("fem.html")
+
+
+if __name__ == "__main__":
+    app.run()
